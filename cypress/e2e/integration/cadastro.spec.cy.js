a45381804913d81e3dee729d1ed711f6a32e9cac
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2022-11-18 10:00:17 -0300

    Update cadastro.spec.cy.js

--- cypress/e2e/integration/cadastro.spec.cy.js
+++ cypress/e2e/integration/cadastro.spec.cy.js
@@ -1,10 +1,14 @@
 describe('Cadastro', () => {
     it('Usuário deve se tornar um entregador', () => {
+        //Setar resolução
         cy.viewport(1440, 900)
+        //Redirecionar navegador para a url
         cy.visit('https://buger-eats.vercel.app')
 
+        //Clicar no botão "Cadastre-se para fazer entregas"
         cy.get('a[href="/deliver"]').click()
-        cy.gey('#page-deliver form h1').should('have.text', 'Cadastre-se para fazer entregas')
+        //Verificar se fomos redirecionados para o formulário de cadastro
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para fazer entregas')
 
     })
 });
