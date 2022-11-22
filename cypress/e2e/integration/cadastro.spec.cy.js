d7c43b87cbad18fef4c037519402002137f3003a
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2022-11-22 00:32:52 -0300

    Preenchendo formulário

--- cypress/e2e/integration/cadastro.spec.cy.js
+++ cypress/e2e/integration/cadastro.spec.cy.js
@@ -8,14 +8,36 @@ describe('Cadastro', () => {
         //Clicar no botão "Cadastre-se para fazer entregas"
         cy.get('a[href="/deliver"]').click()
         //Verificar se fomos redirecionados para o formulário de cadastro
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para fazer entregas')
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
         //Populando dados para testes
         var entregador = {
             nome: 'Fulano de Tal',
             cpf: '12345678910',
             email: 'fulano@hotmail.com',
-            whatsapp: '62981828384'
+            telefone: '62981828384',
+            endereco: {
+                cep: '75389121',
+                rua: 'Rua Mário de Melo',
+                numero: '0',
+                complemento: 'Q27 L16 Casa 3',
+                bairro: 'Setor Cristina II',
+                cidade: 'Trindade/GO',
+            }
         }
+
+        //Preenchendo formulário
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.telefone)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name = "address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name = "address-details"]').type(entregador.endereco.complemento)
+
+
     })
 });
