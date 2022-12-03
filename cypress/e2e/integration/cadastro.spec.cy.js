e41e6edcb9201f9e16c363fafe60f1ae4f88ac65
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2022-12-03 02:49:17 -0300

    Criando cenário "Usuário com CPF inválido"

--- cypress/e2e/integration/cadastro.spec.cy.js
+++ cypress/e2e/integration/cadastro.spec.cy.js
@@ -57,5 +57,81 @@ describe('Cadastro', () => {
 
         //Realizando upload da CNH
         cy.get('input[accept^="image"]').attachFile(entregador.cnh)
+
+        //Clicar no botão de envio de formulário
+        cy.get('button[type="submit"]').click()
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        //Validar aparição da modal
+        cy.get('.swal2-container .swal2-html-container')
+            .should('have.text', expectedMessage)
+
+        //Fechar modal
+        cy.get('.swal2-confirm').click()
+    })
+
+    it('Usuário com CPF inválido', () => {
+        //Setar resolução
+        cy.viewport(1440, 900)
+        //Redirecionar navegador para a url
+        cy.visit('https://buger-eats.vercel.app')
+
+        //Clicar no botão "Cadastre-se para fazer entregas"
+        cy.get('a[href="/deliver"]').click()
+        //Verificar se fomos redirecionados para o formulário de cadastro
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        //Populando dados para testes
+        var entregador = {
+            nome: 'Fulano de Tal',
+            cpf: '123456789AA',
+            email: 'fulano@hotmail.com',
+            telefone: '62981828384',
+            endereco: {
+                cep: '75389121',
+                rua: 'Rua Mário de Melo',
+                numero: '0',
+                complemento: 'Q27 L16 Casa 3',
+                bairro: 'Setor Cristina II',
+                cidade: 'Trindade/GO',
+            },
+            metodo_entrega: 'Bicicleta',
+            cnh: 'cnh-digital.png'
+        }
+
+        //Preenchendo formulário
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.telefone)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name = "address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name = "address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade)
+
+        //Identificando elemento que 'delivery-method li' que contém o contéudo 
+        //de entregador.metodo_entrega (bicicleta) 
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
+        //Seletores CSS
+        //[attribute="value"] -> busca o atributo que contém exatamente o valor
+        //[attribute^="value"] -> busca o atributo que começa com o valor
+        //[attribute$="value"] -> busca o atributo que termina com o valor
+        //[attribute*="value"] -> busca o atributo que contenha a substring do valor  (ex:test, unity test, tests, testing)
+        //[attribute~="value"] -> busca o atributo que contenha a string do valor (ex: test, unity test)
+
+        //Realizando upload da CNH
+        cy.get('input[accept^="image"]').attachFile(entregador.cnh)
+
+        //Clicar no botão de envio de formulário
+        cy.get('button[type="submit"]').click()
+
+        cy.get('span[class="alert-error"]').should('have.text', 'Oops! CPF inválido')
     })
 });
