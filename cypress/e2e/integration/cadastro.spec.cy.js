261235866c6ac2ff25b16f9cdb3e3ef75922300a
Ithalo Azevedo <ithaloazevedoo@gmail.com>
2022-12-05 15:25:21 -0300

    Criando cenário "Usuário com email inválido"

--- cypress/e2e/integration/cadastro.spec.cy.js
+++ cypress/e2e/integration/cadastro.spec.cy.js
@@ -1,7 +1,7 @@
 import SignupPage from '../../../pages/SignupPage'
 
 describe('Cadastro', () => {
-
+    var signup = new SignupPage()
     beforeEach(function () {
         cy.fixture('deliver.json').then(function (d) {
             this.deliver = d
@@ -9,32 +9,26 @@ describe('Cadastro', () => {
     })
 
     it('Usuário deve se tornar um entregador', function () {
-
-
-        var signup = new SignupPage()
-
         signup.go()
         signup.preencherForm(this.deliver.signup)
         signup.submeterForm()
-
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.validarModal(expectedMessage)
-
-
-
         //Fechar modal
         cy.get('.swal2-confirm').click()
     })
 
     it('Usuário com CPF inválido', function () {
-
-
-
-        var signup = new SignupPage()
-
         signup.go()
-        signup.preencherForm(this.deliver.cpf_invalido)
+        signup.preencherForm(this.deliver.invalidCpf)
         signup.submeterForm()
         signup.validarAlertaCPF('Oops! CPF inválido')
+    })
+
+    it('Usuário com email inválido', function () {
+        signup.go()
+        signup.preencherForm(this.deliver.invalidEmail)
+        signup.submeterForm()
+        signup.validarAlertaCPF('Oops! Email com formato inválido.')
     })
 })
